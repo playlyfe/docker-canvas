3c36901d7a043719f683fd8e4fa123ad97c9157a
Johny Jose <johny@playlyfe.com>
2014-12-26 18:12:16 +0530

    fix(core): correct configuration for running canvas in development mode
    Signed-off-by: Johny Jose <johny@playlyfe.com>

--- ansible/playbooks/canvas/roles/postgres/files/init/create_users.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-gosu postgres createuser canvas --no-createdb --no-superuser --no-createrole
-gosu postgres createdb canvas_production --owner=canvas
-gosu postgres createdb canvas_queue_production --owner=canvas
